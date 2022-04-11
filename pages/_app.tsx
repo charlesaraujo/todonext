7bd52a5b4ea05a2477ca8352959b9ab5179900a4
Charles Araújo da Silva <charles_araujo@ymail.com>
2022-04-11 19:19:23 -0300

    new: add adsense

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,6 +9,7 @@ import { SessionProvider } from "next-auth/react";
 import DefaultTheme from "../styles/default.theme";
 import Header from "../components/header";
 import { AuthWrapper } from "../components/authWrapper";
+import Script from "next/script";
 
 // TODO adsense
 // TODO Relacionamento user e todos
@@ -19,6 +20,7 @@ import { AuthWrapper } from "../components/authWrapper";
 // TODO github about
 // TODO add capacitor
 // TODO melhorias lighthouse webpagetest e pwabuilder
+// TODO CSP header https://nextjs.org/docs/api-reference/next.config.js/headers
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [colorScheme, setColorScheme] = useState<ColorScheme>(
@@ -29,25 +31,34 @@ function MyApp({ Component, pageProps }: AppProps) {
     setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
 
   return (
-    <ColorSchemeProvider
-      colorScheme={colorScheme}
-      toggleColorScheme={toggleColorScheme}
-    >
-      <MantineProvider
-        withGlobalStyles
-        withNormalizeCSS
-        theme={{ ...DefaultTheme, colorScheme }}
+    <>
+      <Script
+        id="adsense"
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4491598321720758"
+        async
+        crossOrigin="anonymous"
+      />
+      <script async crossOrigin="anonymous"></script>
+      <ColorSchemeProvider
+        colorScheme={colorScheme}
+        toggleColorScheme={toggleColorScheme}
       >
-        <SessionProvider session={pageProps.session}>
-          <AuthWrapper>
-            <Header />
-            <main>
-              <Component {...pageProps} />
-            </main>
-          </AuthWrapper>
-        </SessionProvider>
-      </MantineProvider>
-    </ColorSchemeProvider>
+        <MantineProvider
+          withGlobalStyles
+          withNormalizeCSS
+          theme={{ ...DefaultTheme, colorScheme }}
+        >
+          <SessionProvider session={pageProps.session}>
+            <AuthWrapper>
+              <Header />
+              <main>
+                <Component {...pageProps} />
+              </main>
+            </AuthWrapper>
+          </SessionProvider>
+        </MantineProvider>
+      </ColorSchemeProvider>
+    </>
   );
 }
 
