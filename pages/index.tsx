ed2b1a7bf06f886f7ea06ef30b0dc057635273c1
Charles Araújo da Silva <charles_araujo@ymail.com>
2022-04-17 22:36:20 -0300

    fix: styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from "next";
-import { Center, Container, Space, Text } from "@mantine/core";
+import { Center, Space, Text } from "@mantine/core";
 import SEO from "../components/seo";
 
 const Home: NextPage<any> = () => {
@@ -9,22 +9,19 @@ const Home: NextPage<any> = () => {
         title="A Fazer"
         description="Cadastre e conclua suas tarefas em qualquer lugar do mundo"
       />
-      <Container>
-        <Center
-          sx={(theme) => ({
-            marginTop: theme.spacing.lg,
-            display: "flex",
-            flexDirection: "column",
-          })}
-        >
-          <Space h="lg" />
-          <Text>
-            Cadastre e conclua suas tarefas em qualquer lugar do mundo
-          </Text>
-          <Space h="lg" />
-          <Text> Conecte-se com o github para começar</Text>
-        </Center>
-      </Container>
+
+      <Center
+        sx={(theme) => ({
+          marginTop: theme.spacing.lg,
+          display: "flex",
+          flexDirection: "column",
+        })}
+      >
+        <Space h="lg" />
+        <Text>Cadastre e conclua suas tarefas em qualquer lugar do mundo</Text>
+        <Space h="lg" />
+        <Text> Conecte-se com o github para começar</Text>
+      </Center>
     </>
   );
 };
